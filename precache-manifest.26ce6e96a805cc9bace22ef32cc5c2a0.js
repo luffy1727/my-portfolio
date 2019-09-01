71a170c57c0deec5560dcc70359cb96fa2f60930
luffy1727 <tushig.tushig@gmail.com>
2019-09-01 15:17:11 +0800

    Updates

---
+++ precache-manifest.26ce6e96a805cc9bace22ef32cc5c2a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d164201a2e4de6be20b8eb5d176cf1e4",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "fcebf5064f73e54d770d",
+    "url": "/my-portfolio/static/css/main.a6969fd3.chunk.css"
+  },
+  {
+    "revision": "8cdb5da583e78248be61",
+    "url": "/my-portfolio/static/js/2.bf2db528.chunk.js"
+  },
+  {
+    "revision": "fcebf5064f73e54d770d",
+    "url": "/my-portfolio/static/js/main.aa32e4ab.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  },
+  {
+    "revision": "01e0cbd7451b818a160616a1d8d81c0c",
+    "url": "/my-portfolio/static/media/avatar.01e0cbd7.png"
+  },
+  {
+    "revision": "0a2d6bfbb423d7d81d106b150989d652",
+    "url": "/my-portfolio/static/media/logo2.0a2d6bfb.png"
+  }
+]);
