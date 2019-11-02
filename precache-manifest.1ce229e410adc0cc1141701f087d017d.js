e79e54021e35a3c3c0b27051a5c21233b22c9657
luffy1727 <tushig.tushig@gmail.com>
2019-11-02 11:41:14 +0800

    Updates

--- precache-manifest.1ce229e410adc0cc1141701f087d017d.js
+++ precache-manifest.1ce229e410adc0cc1141701f087d017d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d1ede3f691fa8dbee2991cad2b6046bf",
+    "revision": "dbf8be0d663ceb222931a4b63baa015f",
     "url": "/my-portfolio/index.html"
   },
   {
-    "revision": "38e03c66641500224c95",
+    "revision": "43315938e5d5043188ed",
     "url": "/my-portfolio/static/css/2.5c17b9f1.chunk.css"
   },
   {
-    "revision": "3731f73e0594afaaaf93",
+    "revision": "f3768eacf1e03b22a12a",
     "url": "/my-portfolio/static/css/main.ff03cd77.chunk.css"
   },
   {
-    "revision": "38e03c66641500224c95",
-    "url": "/my-portfolio/static/js/2.8e3f150b.chunk.js"
+    "revision": "43315938e5d5043188ed",
+    "url": "/my-portfolio/static/js/2.0d480dc3.chunk.js"
   },
   {
-    "revision": "3731f73e0594afaaaf93",
-    "url": "/my-portfolio/static/js/main.7dbaf2eb.chunk.js"
+    "revision": "f3768eacf1e03b22a12a",
+    "url": "/my-portfolio/static/js/main.5bdaa239.chunk.js"
   },
   {
     "revision": "f2d15970f74596a69ca7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.3e5d2f83aa0f246013937e8792503e2f.js"
+  "/my-portfolio/precache-manifest.1ce229e410adc0cc1141701f087d017d.js"
 );
 
 self.addEventListener('message', (event) => {
